622f6e2acacca2ca46eb762b37d66ee11b5207b2
Николай <Doonn3@gmail.com>
2021-12-01 00:27:11 +0300

    Fix

--- Assets/Scripts/PerkSystem/LoadPerksSystem/LoadPerksSystem.cs
+++ Assets/Scripts/PerkSystem/LoadPerksSystem/LoadPerksSystem.cs
@@ -33,8 +33,8 @@ public static class LoadPerksSystem
 
         for (int i = 0; i < returnCountPerk; i++)
         {
-            int randomindex = Random.Range(0, AllPerks.Length);
-            var getPerkFromIndex = AllPerks[randomindex];
+            int randomIndex = Random.Range(0, AllPerks.Length);
+            var getPerkFromIndex = AllPerks[randomIndex];
 
             if (!perks.Contains(getPerkFromIndex))
             {
