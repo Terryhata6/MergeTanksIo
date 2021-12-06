9cac5eb3caa1bc565b396c6a55dafcad38e3bd5f
ENTER-ALT <rodustish88@mail.ru>
2021-12-06 23:26:07 +0300

    Com

--- Assets/Scripts/Model/PlayerState/PlayerMoveStateModel.cs
+++ Assets/Scripts/Model/PlayerState/PlayerMoveStateModel.cs
@@ -20,15 +20,14 @@ public class PlayerMoveStateModel : BasePlayerStateModel
     _tempVector.x = _vectorMove2D.x;
     _tempVector.y = 0;
     _tempVector.z = _vectorMove2D.y;
-    //<<Alt_Enter
-    // Vector3 vectorDirection = new Vector3(_vectorMove2D.x, 1f, _vectorMove2D.y);
-    // player.transform.rotation = Quaternion.Slerp(
-    //   player.transform.rotation,
-    //   Quaternion.LookRotation(vectorDirection),
-    //   Time.deltaTime * player.ViewParams.RotationSpeed);
-    // player.transform.position += player.transform.forward * Time.deltaTime * player.ViewParams.MoveSpeed;
-    //>>END
-    player.Rotation = Quaternion.LookRotation(_tempVector, Vector3.up);
-    player.Transform.Translate(Vector3.forward * _magnitude * 0.01f * player.ViewParams.MoveSpeed * Time.deltaTime);
+    player.transform.rotation = Quaternion.Slerp(
+      player.transform.rotation,
+      Quaternion.LookRotation(_tempVector),
+      Time.deltaTime * player.ViewParams.RotationSpeed);
+    if (_tempVector.Equals(Vector3.zero))
+    {
+      return;
+    }
+    player.transform.position += player.transform.forward * Time.deltaTime * player.ViewParams.MoveSpeed;
   }
 }
